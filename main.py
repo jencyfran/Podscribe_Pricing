b80a81557ddc864592e0a0b1fd41196567bebd6f
jency <jencyfrancisxavier@gmail.com>
2023-10-03 16:47:42 -0700

    This is the web application for automated internal pricing tool

--- main.py
+++ main.py
@@ -130,5 +130,5 @@ def index():
     return render_template('index.html', result=result, message=message, error=error)
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
