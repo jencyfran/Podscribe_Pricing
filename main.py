3e89bea256c942ab4c04a0abcd8fe8cf8f24dc93
jency <jencyfrancisxavier@gmail.com>
2023-10-03 12:30:57 -0700

    This is the web application for automated internal pricing tool

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from flask import Flask, render_template, request
 import pandas as pd
-import xlrd
-import os
 
 app = Flask(__name__)
 
@@ -14,8 +12,9 @@ def cal_pricing(impression_cap):
 
         # Data cleaning
         # Convert columns to string and replace symbols
-        for column in ['Monthly Rate (Min)', 'Monthly Rate (Max)', 'Monthly Attribution Impression Cap']:
-            df[column] = pd.to_numeric(df[column].astype(str).str.replace('[$,]', '', regex=True), errors='coerce')
+        for column in df.columns:
+            if df[column].dtype == 'O':  # Check if the column is string type
+                df[column] = pd.to_numeric(df[column].astype(str).str.replace('[$,]', '', regex=True), errors='coerce')
 
         # Ensure 'Monthly Attribution Impression Cap' is int type
         df = df.dropna(subset=['Monthly Attribution Impression Cap'])
@@ -33,7 +32,19 @@ def cal_pricing(impression_cap):
         annual_rate_low = tier_row['Monthly Rate (Min)'] * 12
         annual_rate_high = tier_row['Monthly Rate (Max)'] * 12
 
+        # Extract additional charges and replace NaN with 0 and TRUE with Included
+        additional_charges = {
+            "Planning Included": tier_row['Planning Included?'],
+            "G Sheet Sync Included": tier_row['G Sheet Sync Included?'],
+            "Brand Safety Add-on Package": tier_row['Brand Safety Add-on Package'],
+            "Dedicated Support Slack Channel": tier_row['Dedicated Support Slack Channel'],
+            "Publisher Growth Package": tier_row['Publisher Growth Package']
+        }
+        additional_charges = {key: (0 if pd.isna(value) else 'Included' if value == 'TRUE' else value)
+                              for key, value in additional_charges.items()}
+
         return {
+            "Impression Cap": impression_cap,
             "Tier": tier_row['Tier'],
             "Annual Rate (Low)": annual_rate_low,
             "Annual Rate (High)": annual_rate_high,
@@ -41,7 +52,9 @@ def cal_pricing(impression_cap):
             "Monthly Rate (High)": tier_row['Monthly Rate (Max)'],
             "CPM (Low)": cpm_low,
             "CPM (High)": cpm_high,
+            "Additional Charges": additional_charges
         }
+
     except Exception as e:
         # If there's an error anywhere in the function, it will be caught here and returned as a string
         return str(e)
@@ -49,23 +62,32 @@ def cal_pricing(impression_cap):
 
 def generate_readable_output(result):
     try:
-        # Template for the email/message content
         message_template = (
             f"Hello [Client Name],\n\n"
-            f"Thank you for considering our service. Based on a monthly attribution impression cap of [Impression Cap], "
-            f"you fall into our Tier {result['Tier']} pricing. Here are the details of this pricing tier:\n\n"
+            f"Thank you for considering our service. Based on a monthly attribution impression cap of [Impression Cap],"
+            f" you fall into our Tier {result['Tier']} pricing. Below are the detailed pricing and features "
+            f"available:\n\n"
             f"- Annual Rate: ${result['Annual Rate (Low)']} to ${result['Annual Rate (High)']}\n"
             f"- Monthly Rate: ${result['Monthly Rate (Low)']} to ${result['Monthly Rate (High)']}\n"
-            f"- Cost Per Mille (CPM): ${result['CPM (Low)']} to ${result['CPM (High)']}\n\n"
-            f"Feel free to contact us if you have any questions or need further assistance.\n\n"
-            f"Best Regards,\n"
-            f"Jency Francis Xavier - Podscribe"
+            f"- Cost Per Mile (CPM): ${result['CPM (Low)']} to ${result['CPM (High)']}\n\n"
+            f"Additional Features and Charges:\n"
         )
 
-        # Replace placeholders with actual values (if needed)
+        for charge, value in result['Additional Charges'].items():
+            # Handling None and 'TRUE' values
+            if pd.isna(value) or value == "TRUE":
+                message_template += f"  - {charge}: Included\n"
+            else:
+                message_template += f"  - {charge}: ${value}\n"
+
+        message_template += (
+            f"\nFeel free to contact us if you have any questions or need further assistance.\n\n"
+            f"Best Regards,\n"
+            f"Jency Francis - Podscribe"
+        )
+
         message = message_template.replace("[Client Name]", "Valued Client")
-        message = message.replace("[Impression Cap]", "500,000")  # Example value
-        message = message.replace("[Jency Francis]", "Podscribe")
+        message = message.replace("[Impression Cap]", f"{result['Impression Cap']:,}")
 
         return message
     except Exception as e:
@@ -82,13 +104,8 @@ def index():
         try:
             impression_cap = int(request.form['impression_cap'])
             result = cal_pricing(impression_cap)
-            print("Result:", result)  # Debugging line
 
-            # Generate the message using the results
-            if isinstance(result, dict) and 'Tier' in result:
-                message = generate_readable_output(result)
-            else:
-                error = "Unexpected result format."
+            message = generate_readable_output(result)
 
         except ValueError:
             error = "Invalid impression cap."
