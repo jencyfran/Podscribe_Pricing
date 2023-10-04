f5c4252e013c54501f84dda58a7dabd7f28cc271
jency <jencyfrancisxavier@gmail.com>
2023-10-03 19:32:33 -0700

    This is the web application for automated internal pricing tool

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 import pandas as pd
-
+import os
 app = Flask(__name__)
 
 
@@ -131,4 +131,4 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
